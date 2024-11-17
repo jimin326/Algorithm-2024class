01ac85a3c5c210443b4ecbec91ed9507fffba979
jimin <jimincheon76@gmail.com>
2024-11-18 04:03:10 +0900

    ch9 QuickSort_21번문제 오류 수정

--- DS_ch09_CJM/ch09_04/quickSort2.c
+++ DS_ch09_CJM/ch09_04/quickSort2.c
@@ -9,21 +9,18 @@ int partition(int a[], int begin, int end, int size) {
 	R = end;
 	pivot = begin;	// 정렬 범위에서 첫번째 원소 begin을 피봇으로 선택
 	printf("\n [%d단계 : pivot = %d ] \n", ++i, a[pivot]);
-	while (L < R) {
-		while ((a[L] < a[pivot]) && (L < R)) L++;
-		while ((a[R] >= a[pivot]) && (L < R)) R--;
+	while (L <= R) {
+		while (L <= end && a[L] < a[pivot]) L++;
+		while (R >= begin && a[R] > a[pivot]) R--;		//  a[R] >= a[pivot]가 아님
+		// while ((a[L] < a[pivot]) && (L < R)) L++;    //  L과 R이 교차되는 경우 배제하는 오류
+		// while ((a[R] > a[pivot]) && (L < R)) R--;   //  L과 R이 교차되는 경우 배제하는 오류
 		if (L < R) {			// L과 R 원소의 자리 교환
 			temp = a[L];
 			a[L] = a[R];
 			a[R] = temp;
-
-			// if(L == pivot)   pivot = R; // L이 피봇인 경우, 변경된 피봇의 위치(R)를 다시 저장!
-			//////////////////////////////////////////////////////////////// 9장 연습문제 21번 풀이용
-			if ((L + 1) == R) { R = L; break;}	// L와 R이 인접한 경우, 작업 중단하고 피봇과 자리교환
-		}
+		}	
 	}
 
-	// (L=R)이 된경우, 더 이상 진행할 수 없으므로 R 원소와 피봇 원소의 자리를 교환하여 마무리
 	temp = a[pivot];
 	a[pivot] = a[R];
 	a[R] = temp;
@@ -37,7 +34,7 @@ void quickSort(int a[], int begin, int end, int size) {
 	int p;
 	if (begin < end){
 		p = partition(a, begin, end, size);		// 피봇의 위치에 의해 분할 위치 결정
-		quickSort(a, begin, p - 1, size);		// 피봇의 왼쪽 부분집합에 대해 퀵 정렬을 재귀호출
-		quickSort(a, p + 1, end, size);			// 피봇의 오른쪽 부분집합에 대해 퀵 정렬을 재귀호출
+		if (begin < p - 1) quickSort(a, begin, p - 1, size);
+		if (p + 1 < end) quickSort(a, p + 1, end, size);
 	}
 }
